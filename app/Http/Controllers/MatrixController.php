fe780791aface1672b6ad8e3329605a5fae44e33
assemjimzawi <assem.cs.90@gmail.com>
2021-01-12 23:13:18 +0200

    make PSR-2 coding standard to code and add Strict type hinting

--- app/Http/Controllers/MatrixController.php
+++ app/Http/Controllers/MatrixController.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 
@@ -16,20 +18,21 @@ class MatrixController extends Controller
     }
 
     /*** Declare a function to calculate Matrix Multiplication ***/
-    public function matrixMultiplication( ){
+    public function matrixMultiplication( )
+    {
 
         // Initialize matrix a
         $a = array(  
-            array(1, 3, 2),  
-            array(3, 1, 1),  
-            array(1, 2, 2)  
+             array(1, 3, 2),  
+             array(3, 1, 1),  
+             array(1, 2, 2)  
           );  
           
           // Initialize matrix b
-          $b = array(  
-           array(2, 1, 1),  
-           array(1, 0, 1),  
-        //    array(1, 3, 1)  
+        $b = array(  
+             array(2, 1, 1),  
+             array(1, 0, 1),  
+             array(1, 3, 1)  
          );  
 
          //Calculates number of rows and columns present in first matrix  
@@ -43,28 +46,28 @@ class MatrixController extends Controller
 
          //For two matrices to be multiplied,   
          //number of columns in first matrix must be equal to number of rows in second matrix 
-         if($col1 != $row2){  
+         if ($col1 != $row2) {  
             return response()->json([
-                    'error' => "Matrices cannot be multiplied",
+                   'error' => "Matrices cannot be multiplied",
             ]);
-        }  
-        else{  
+         } else { 
+          
             //Array prod will hold the result and initialize it with 0  
             $prod = array_fill(0, $col2, array_fill(0, $row1, 0));  
 
               
             //Performs product of matrices a and b. Store the result in matrix prod  
-            for($i = 0; $i < $row1; $i++){  
-                for($j = 0; $j < $col2; $j++){  
-                    for($k = 0; $k < $row2; $k++){  
+            for ($i = 0; $i < $row1; $i++) {  
+                for ($j = 0; $j < $col2; $j++) {  
+                    for ($k = 0; $k < $row2; $k++) {  
                        $prod[$i][$j] = $prod[$i][$j] + $a[$i][$k] * $b[$k][$j];   
                     }  
                 }  
             }  
               
             print("Product of two matrices: <br>");  
-            for($i = 0; $i < $row1; $i++){  
-                for($j = 0; $j < $col2; $j++){ 
+            for ($i = 0; $i < $row1; $i++) {  
+                for ($j = 0; $j < $col2; $j++) { 
                     
                   $charcter = chr($prod[$i][$j]+65);
                   print($charcter . " ");  
@@ -72,7 +75,7 @@ class MatrixController extends Controller
                 print("<br>");  
         
         }
-}
+         }
 
-}  
+    }  
 }
